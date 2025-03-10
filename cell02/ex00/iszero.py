87bee685f91aa024091e6f8f1dc1dd20a9fc4c08
bank2779 <40773@prot.ac.th>
2025-03-10 14:50:02 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,11 @@
+number = input("Enter a number: ")
+
+try:
+    num = int(number)
+    
+    if num == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
