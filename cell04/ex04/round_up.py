17a035f8818cfc90bbc8856c909fc2ca702ffaca
bank2779 <40773@prot.ac.th>
2025-03-11 10:35:07 +0700

    Add files via upload

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import math
+
+# Ask the user for a number
+number = float(input("Give me a number: "))
+
+# Round the number up and display the result
+rounded_number = math.ceil(number)
+print(rounded_number)
