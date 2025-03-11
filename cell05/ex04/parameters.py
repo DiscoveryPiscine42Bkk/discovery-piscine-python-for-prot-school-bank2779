7c2b63940ed256b30f91f53867bd3f10b7ae0840
bank2779 <40773@prot.ac.th>
2025-03-11 12:21:17 +0700

    Add files via upload

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,7 @@
+import sys
+
+# Count the number of parameters passed, excluding the script name
+num_parameters = len(sys.argv) - 1
+
+# Display the result
+print(f"Number of parameters: {num_parameters}.")
