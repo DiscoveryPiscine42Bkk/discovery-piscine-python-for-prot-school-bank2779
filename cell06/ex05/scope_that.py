094271593868d8c7b7ffcaa84be56f6922380436
bank2779 <40773@prot.ac.th>
2025-03-11 14:33:29 +0700

    Add files via upload

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,12 @@
+def add_one(number):
+    return number + 1
+
+# Initialize a variable
+num = 5
+print("Before:", num)
+
+# Call the method and store the result
+num = add_one(num)
+
+# Display the variable again
+print("After:", num)
